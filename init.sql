89959419bf4450ee153539d88c6a0ee1d99beb66
martin76ec <larreaamartin@gmail.com>
2023-08-09 12:01:00 -0500

    feat: add tables

--- init.sql
+++ init.sql
@@ -6,60 +6,65 @@ GRANT ALL PRIVILEGES ON library.* TO 'librarian'@'%';
 
 USE library;
 
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `category_name` varchar(255) NOT NULL
+CREATE TABLE IF NOT EXISTS login_credentials
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `authors` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL
+CREATE TABLE IF NOT EXISTS `librarians`
+(
+    `id`         int          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `first_name` varchar(255) NOT NULL,
+    `last_name`  varchar(255) NOT NULL,
+    login        INT          NOT NULL,
+    FOREIGN KEY (`login`) REFERENCES `login_credentials` (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `books` (
-  `isbn` varchar(13) NOT NULL PRIMARY KEY,
-  `name` varchar(255) NOT NULL,
-  `author` int NOT NULL,
-  `category` int NOT NULL,
-  `price` int(11) NOT NULL,
-  `year_of_edition` int(4) NOT NULL,
-  FOREIGN KEY (`category`) REFERENCES `categories` (`id`),
-  FOREIGN KEY (`author`) REFERENCES `authors` (`id`)
+CREATE TABLE IF NOT EXISTS `books`
+(
+    `isbn`            varchar(13)  NOT NULL PRIMARY KEY,
+    `name`            varchar(255) NOT NULL,
+    `author`          varchar(255) NOT NULL,
+    `category`        varchar(255) NOT NULL,
+    `price`           int(11)      NOT NULL,
+    `year_of_edition` int(4)       NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `clients`
+(
+    `id`         INT AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(255) NOT NULL,
+    `last_name`  VARCHAR(255) NOT NULL,
+    `address`    VARCHAR(255),
+    `phone`      VARCHAR(20),
+    `login`      INT          NOT NULL,
+    FOREIGN KEY (`login`) REFERENCES `login_credentials` (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `borrows` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `isbn` varchar(13) NOT NULL,
-  `user` int NOT NULL,
-  `borrow_start_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `borrow_end_date` datetime,
-  `returned` tinyint(1) NOT NULL DEFAULT 0,
-  FOREIGN KEY (`isbn`) REFERENCES `books` (`isbn`),
-  FOREIGN KEY (`user`) REFERENCES `users` (`id`)
+CREATE TABLE IF NOT EXISTS `borrows`
+(
+    `id`                int         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `isbn`              varchar(13) NOT NULL,
+    `client`            int         NOT NULL,
+    `borrow_start_date` datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `borrow_end_date`   datetime,
+    `returned`          tinyint(1)  NOT NULL DEFAULT 0,
+    FOREIGN KEY (`isbn`) REFERENCES `books` (`isbn`),
+    FOREIGN KEY (`client`) REFERENCES `clients` (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `borrow_rules` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `rule_name` varchar(255) NOT NULL,
-  `value` varchar(255) NOT NULL,
-  `type` enum('string', 'numeric') NOT NULL
-);
+INSERT INTO login_credentials (email, password) VALUES ('client@example.com', 'clientpassword');
+INSERT INTO login_credentials (email, password) VALUES ('librarian@example.com', 'librarianpassword');
+INSERT INTO clients (first_name, last_name, address, phone, login) VALUES ('John', 'Doe', '123 Main St', '555-1234', 1);
+INSERT INTO clients (first_name, last_name, address, phone, login) VALUES ('Jane', 'Smith', '456 Elm St', '555-5678', 2);
+INSERT INTO librarians (first_name, last_name, login) VALUES ('Alice', 'Johnson', 2);
+INSERT INTO librarians (first_name, last_name, login) VALUES ('Bob', 'Williams', 1);
+INSERT INTO books (isbn, name, author, category, price, year_of_edition)
+VALUES ('9780451450522', 'Neuromancer', 'William Gibson', 'Science Fiction', 15, 1984);
+INSERT INTO books (isbn, name, author, category, price, year_of_edition)
+VALUES ('9780062459367', 'To Kill a Mockingbird', 'Harper Lee', 'Classic', 10, 1960);
+INSERT INTO books (isbn, name, author, category, price, year_of_edition)
+VALUES ('9780143110425', '1984', 'George Orwell', 'Dystopian', 12, 1949);
 
-CREATE TABLE IF NOT EXISTS `user_borrows` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `borrow` int NOT NULL,
-  `rule` int NOT NULL,
-  FOREIGN KEY (`borrow`) REFERENCES `borrows` (`id`),
-  FOREIGN KEY (`rule`) REFERENCES `borrow_rules` (`id`)
-);
